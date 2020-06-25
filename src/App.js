6f81ac82c771f99dce755f0587b387a99033e2ff
sebak lamsal <sebak.lamsal41@student.qcc.cuny.edu>
2020-06-25 15:04:48 -0400

    after changes

--- src/App.js
+++ src/App.js
@@ -4,68 +4,38 @@ class Counter extends Component {
         super();
         this.state = {
             count: 0,
-            doubleCount: false,
         };
     }
     increment = () => {
-        if (this.state.count < 20) {
-            this.setState({
-                count: this.state.count + 1
-            })
-        }
-    }
-    decrement = () => {
-        if (this.state.count > 0) {
-            this.setState({
-                count: this.state.count - 1
-
-            })
-        }
-    }
-    clear = () => {
         this.setState({
-            count: 0,
+            count: this.state.count + 1
         })
-    }
-    double = () => {
-        if (this.state.doubleCount) {
-            this.setState({
-                coubleCount: true,
-            })
-            console.log(this.state.doubleCount)
-        }
+
     }
 
     render() {
         return ( <
             div className = "container" >
+
+
             <
-            div className = "navbar" > Counter.js < /div>    <
+            div classNAme = "navbar" > Counter.js < /div>   <
             div className = "counter" >
 
 
 
+            <
+            h1 > { this.state.count } < /h1>    <
+            button type = "button"
+            onClick = { this.increment } > increment < /button>   <
+            button type = "button" > Decrement < /button>   <
+            div >
 
             <
-            h1 > { this.state.count } < /h1>     <
-            button type = "button"
-            onClick = { this.increment } > increment < /button>    <
-            button type = "button"
-            onClick = { this.decrement } > Decrement < /button>  <
-            button type = "button"
-            onClick = { this.clear } > clear < /button> <
-            button type = "button"
-            onClick = { this.double } > {
-                this.state.doubleCount ? "Double Count" : 'Single Count" } <
-                    /button>  < /
-                div >
-
-
-                <
-                /div>
-            );
-        };
-
+            /div>
+        );
     };
 
-    export default Counter;
+};
+
+export default Counter;
